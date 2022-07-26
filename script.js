256b94bfefc1ba9b07c46d3f22b3ef36948ffc8e
Mahoroa2011 <marxcfm@gmail.com>
2022-07-25 21:54:01 -0500

    updated to include reason for failure.

--- script.js
+++ script.js
@@ -15,3 +15,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//im sorry i didnt get anything done. i may be high functioning autistic but that doesnt mean i can handle everything i was supposed to this week or the last one. too many things kept coming up and i kept resorting to coping mechanisms to help process all the stress instead of getting work done. by the time i finally asked for help it was too late and my step dad who works in the field wouldnt even help me.
+//i will try to do better this coming week but i make no promises. online learning is way more difficult for me to do than actually learning in a classroom. 
