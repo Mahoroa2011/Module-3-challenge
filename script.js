5050176f83e232f74a42e3b391eccda28d5b6f20
Mahoroa2011 <108108184+Mahoroa2011@users.noreply.github.com>
2022-08-15 16:03:37 -0500

    Added prompts for variables

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+var uppercaselist =["A","B"]
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,6 +13,34 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+var lengthprompt = prompt ("Password Length")
+
+if (lengthprompt<8){
+  alert("Must be greater than 8")
+  return
+}
+if (lengthprompt>128){
+  alert("Must be 128 or less")
+  return
+}
+if (isNaN(lengthprompt)){
+  alert("Please use Numbers")
+  return
+}
+
+var lowercaseprompt = confirm ("Include Lowercase?")
+
+var uppercaseprompt = confirm ("Include Uppercase?")
+
+var includenumbers = confirm ("Include Numbers?")
+
+var includesymbols = confirm ("Include Symbols?")
+
+if (lowercaseprompt===true){
+  
+}
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
