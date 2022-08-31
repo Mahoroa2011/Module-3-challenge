8639716b80dfa868f4b90513f520ec202dddeecb
Mahoroa2011 <108108184+Mahoroa2011@users.noreply.github.com>
2022-08-30 20:53:52 -0500

    completed it

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var uppercaselist =["A","B"]
+var uppercaselist =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowercaselist =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numberlist =["0","1","2","3","4","5","6","7","8","9"]
+var symbolslist =["!","@","#","$","%","^","&","*","?","."]
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -28,7 +31,7 @@ if (isNaN(lengthprompt)){
   alert("Please use Numbers")
   return
 }
-
+var possiblecharacters =[]
 var lowercaseprompt = confirm ("Include Lowercase?")
 
 var uppercaseprompt = confirm ("Include Uppercase?")
@@ -38,9 +41,35 @@ var includenumbers = confirm ("Include Numbers?")
 var includesymbols = confirm ("Include Symbols?")
 
 if (lowercaseprompt===true){
-  
+ possiblecharacters= possiblecharacters.concat(lowercaselist)
 }
+
+if (uppercaseprompt===true){
+  possiblecharacters= possiblecharacters.concat(uppercaselist)
 }
+
+if (includenumbers===true){
+  possiblecharacters= possiblecharacters.concat(numberlist)
+}
+
+if (includesymbols===true){
+  possiblecharacters= possiblecharacters.concat(symbolslist)
+}
+
+console.log(possiblecharacters)
+console.log(possiblecharacters[0])
+console.log(possiblecharacters.length)
+var generatedpassword="";
+for(var i=0;i<lengthprompt;i++)
+{
+  generatedpassword+=possiblecharacters[Math.floor(Math.random()*possiblecharacters.length)]
+}
+console.log(generatedpassword)
+
+return generatedpassword;
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
